3bbbf6dd4061cfcd2f012632b747937737103a91
satyam-yadav12 <satyam.dev637@gmail.com>
2026-02-20 13:29:11 +0530

    fix: skeleton loader

--- Lumen-Frontend/src/components/SkeletonLoader.jsx
+++ Lumen-Frontend/src/components/SkeletonLoader.jsx
@@ -1,4 +1,4 @@
-const SkeletonLoader = ({ colCount = 3, refProp }) => {
+const SkeletonLoader = () => {
   const randomHeights = [250, 270, 320, 400, 420, 300];
   const boxes = Array.from({ length: 9 }, (_, i) => `box${i + 1}`);
 
@@ -7,31 +7,18 @@ const SkeletonLoader = ({ colCount = 3, refProp }) => {
     return randomHeights[idx];
   };
 
-  // split boxes into columns
-  const columns = Array.from({ length: colCount }, () => []);
-  boxes.forEach((box, idx) => {
-    columns[idx % colCount].push(box);
-  });
-
   return (
-    <div className="flex flex-row gap-4 px-4">
-      {columns.map((col, colIndex) => (
-        <div key={colIndex} className="flex flex-col">
-          {col.map((box, idx) => {
-            const height = getRandomHeight();
-            // add ref only to first skeleton box
-            const isFirst = colIndex === 0 && idx === 0;
-            return (
-              <div
-                key={box}
-                className="group animate-pulse bg-gray-300 rounded-xl my-4 break-inside-avoid w-[90vw] sm:w-[45vw] lg:w-[30vw] h-max"
-                style={{ height: `${height}px` }}
-                ref={isFirst ? refProp : null}
-              ></div>
-            );
-          })}
-        </div>
-      ))}
+    <div className="coulumns-1 sm:columns-2 lg:columns-3 max-screen overflow-hidden gap-4 px-4">
+      {boxes.map((val) => {
+        const height = getRandomHeight();
+        return (
+          <div
+            key={val}
+            className="group animate-pulse bg-gray-300 rounded-xl mb-6 break-inside-avoid w-[90vw] sm:w-[45vw] lg:w-[30vw] h-max"
+            style={{ height: `${height}px` }}
+          ></div>
+        );
+      })}
     </div>
   );
 };
